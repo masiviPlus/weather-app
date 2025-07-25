bbfbb3739ea74e17b9d013b9bca05f8fc5ec4e3d
Nicholas <141828283+masiviPlus@users.noreply.github.com>
2025-07-25 19:00:50 +0400

    API integration/Test

--- my-weather-app/src/app/page.tsx
+++ my-weather-app/src/app/page.tsx
@@ -2,9 +2,18 @@ import Image from "next/image";
 
 
 export default function Home() {
+  const apiKey = process.env.NEXT_PUBLIC_WEATHER_API_KEY;
+  const city = "Tbilisi";
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
+
+  fetch(url)
+  .then(res => res.json())
+  .then(data => console.log(data));
+
   return (
     <div className="min-h-screen bg-cyan-950 text-white">
       HELLO WORLD
+      Heelooajkdoiwj
     </div>
   );
 }
